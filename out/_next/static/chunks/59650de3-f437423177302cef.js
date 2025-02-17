117c3e49b9ad3bcf33a4ef4565e6cbf4e80d8dc9
Suarez Matias <be.creativedesing@gmail.com>
2025-02-17 09:26:53 -0300

    footer

--- out/_next/static/chunks/59650de3-f437423177302cef.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[150],{2950:(t,r,e)=>{e.d(r,{zrT:()=>o});var n=e(5816);function o(t){return(0,n.k5)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"m6 9 6 6 6-6"},child:[]}]})(t)}}}]);
